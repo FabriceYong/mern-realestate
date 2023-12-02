4a693343407865ffc75da4203ff0cb50a6d8242a
Fabrice Yong <fabricenjua@gmail.com>
2023-12-02 02:15:00 +0100

    database setup

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
 
 const app = express()
+
+mongoose.connect('mongodb+srv://fabricenjua:njua1234@real-estate-app.8eain6e.mongodb.net/').then(() => {
+    console.log('connected to database')
+}).catch((err) => {
+    console.log(err, err.message)
+})
 
 app.listen(5000, () => console.log('Server is running on port 5000!'))
