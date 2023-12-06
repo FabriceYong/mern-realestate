a0b232360d3abef037ea231aa2b063412ee32def
Fabrice Yong <fabricenjua@gmail.com>
2023-12-06 14:56:24 +0100

    create middleware and error handler function

--- api/api_controllers/auth_controller.js
+++ api/api_controllers/auth_controller.js
@@ -1,10 +1,20 @@
-import User from "../models/userModel.js"
+import bcrypt from 'bcrypt'
+import User from '../models/userModel.js'
+import { errorHandler } from '../utils/error.js'
 
-export const signup = async (req, res) => {
-    const { username, email, password} = req.body
-    const newUser = new User({
-        username, email, password
-    })
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body
+  const hashedPassword = bcrypt.hashSync(password, 10)
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  })
+  try {
     await newUser.save()
-    res.status(200).json(newUser)
+    res.status(201).json(newUser)
+  } catch (err) {
+    // next(errorHandler(500, 'Duplicate username or password'))
+    next(err)
+  }
 }
--- api/index.js
+++ api/index.js
@@ -8,15 +8,29 @@ import authRouter from './api_routers/auth_route.js'
 
 const app = express()
 
+// middlewares 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+// error middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode ?? 500
+    const message = err.message ?? "Internal Server Error"
 
-mongoose.connect('mongodb+srv://fabricenjua:njua1234@real-estate-app.8eain6e.mongodb.net/').then(() => {
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
+
+// database connection string
+mongoose.connect('mongodb+srv://fabricenjua:njua1234@real-estate-app.8eain6e.mongodb.net/mern-estate?retryWrites=true&w=majority').then(() => {
     console.log('connected to database')
 }).catch((err) => {
     console.log(err, err.message)
 })
 
+// api routes
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 
